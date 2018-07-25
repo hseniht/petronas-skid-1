a885f3dab9ee556f68eb6ffbcd0b62bac5ff7920
hseniht <thinesh_kumar_mu@hotmail.com>
2018-07-25 12:04:00 +0800

    Done with Report Page -added custom.js

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+console.log("custom.js loaded");
+
+//to show sample-record-table
+$("#sample-report-table-1").hide();
+
+$("#getRecord").click(function(){
+    $("#sample-report-table-1").fadeIn(500);
+});
