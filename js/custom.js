b17bc8036c5bf51fb46331c81575448b7737ac60
hseniht <thinesh_kumar_mu@hotmail.com>
2018-07-25 17:41:41 +0800

    Added TableEdit plugin - added tabledit.js file - added tk-live-alarm-2.html

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,21 @@ $("#sample-report-table-1").hide();
 $("#getRecord").click(function(){
     $("#sample-report-table-1").fadeIn(500);
 });
+
+
+
+
+$('#example23').Tabledit({
+    deleteButton: false,
+    columns: {
+        identifier: [0, 'id'],
+        editable: [[1, 'Tank']]
+    }
+});
+$('#example-2').Tabledit({
+deleteButton: false,
+    columns: {
+        identifier: [0, 'id'],
+        editable: [[2, 'Last']]
+    }
+});
